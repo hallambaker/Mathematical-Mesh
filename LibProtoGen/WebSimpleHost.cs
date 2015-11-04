64a225ce15678a724f15cab357733a67af7784fb
Hallam <hallam@gmail.com>
2015-11-04 17:22:07 -0500

    Host container for Web abd UDP access.

--- LibProtoGen/WebSimpleHost.cs
+++ LibProtoGen/WebSimpleHost.cs
@@ -5,6 +5,52 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Goedel.Protocol {
-    class WebSimpleHost {
+
+
+    public class PortRegistration {
+
+        }
+
+
+    public class HostRegistration {
+        JPCServer JPCServer;
+        JPCHost JPCHost;
+
+        public HostRegistration (JPCServer JPCServer, JPCHost JPCHost) {
+            this.JPCServer = JPCServer;
+            }
+
+        public PortRegistration AddHTTP () {
+            return null;
+            }
+
+        public PortRegistration AddUDP() {
+            return null;
+            }
+
+        }
+
+
+    public class JPCServer {
+
+        public JPCServer() {
+
+            }
+
+        public HostRegistration Add(JPCHost JPCHost) {
+            var Registration = new HostRegistration(this, JPCHost);
+
+            return Registration;
+            }
+
+        public void Start () {
+
+            }
+
+        public void Stop () {
+
+            }
+
+
         }
     }
